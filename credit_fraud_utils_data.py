7b1b86e492397970157635b063807a3f1d984b76
ahmeddiab1234 <shhth0034@gmail.com>
2025-09-04 19:15:32 +0300

    working

--- credit_fraud_utils_data.py
+++ credit_fraud_utils_data.py
@@ -15,7 +15,7 @@ processing:
 """
 import pandas as pd
 from numpy import where
-from utils.helper_fun import load_data
+from utils.helper_fun import load_df, load_x_t, split_data
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, Normalizer
 from collections import Counter
 from imblearn.under_sampling import RandomUnderSampler
@@ -23,100 +23,143 @@ from imblearn.over_sampling import SMOTE, RandomOverSampler
 
 RANDOM_STATE=42
 
-class Preprocessing():
-    def __init__(self, x:pd.DataFrame,y:pd.DataFrame):
-        self.x=x
-        self.y=y
+import pandas as pd
+import numpy as np
+from collections import Counter
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, Normalizer
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import SMOTE, RandomOverSampler
+from imblearn.pipeline import Pipeline
 
-    def remove_dublicates(self, drop=True):
+RANDOM_STATE = 42
+
+class Preprocessing:
+    def __init__(self, df: pd.DataFrame):
+        self.df=df
+
+    def remove_duplicates(self, drop=True):
         if drop:
-            self.x.drop_duplicates(inplace=True)
-        return x
-    
-    def handle_outliers(self, drop=True):
-        for col in self.x.columns:
-            q1 = self.x[col].quantile(0.25)
-            q3 = self.x[col].quantile(0.75)
-            iqr = q3-q1
+            self.df.drop_duplicates(inplace=True)
+        return self.df
 
-            lower = q1-(1.5*iqr)
-            upper = q3+(1.5*iqr)
+    def handle_outliers(self, drop=True):
+        cols = self.df.columns[:-1]
+        for col in cols:
+            q1 = self.df[col].quantile(0.25)
+            q3 = self.df[col].quantile(0.75)
+            iqr = q3 - q1
+
+            lower = q1 - (1.5 * iqr)
+            upper = q3 + (1.5 * iqr)
 
             if drop:
-                self.x = self.x[(df[col]>=lower)|(self.x[col]<=upper)]
+                self.df = self.df[(self.df[col] >= lower) & (self.df[col] <= upper)]
             else:
-                self.x = where(self.x[col]<lower, lower, where(self.x[col]>upper, upper, self.x[col]))
-        return x
-
+                self.df[col] = np.where(
+                    self.df[col] < lower, lower,
+                    np.where(self.df[col] > upper, upper, self.df[col])
+                )
+        return self.df
 
     def change_time_to_hours(self, col):
-        self.x[col] = self.x[col]/60/60
-        return x
-
+        self.df[col] = self.df[col] / 3600.0  
+        return self.df
 
     def scaling(self, option=1):
-        if option==1:
+        if option == 1:
             scaler = MinMaxScaler()
-        elif option==2:
+        elif option == 2:
             scaler = StandardScaler()
-        elif option==3:
+        elif option == 3:
             scaler = Normalizer()
         else:
             return None
+        cols = self.df.columns[:-1]
         
-        x_transformed = scaler.fit_transform(self.x)
-        return scaler, x_transformed
-    
+        df[cols] = scaler.fit_transform(df[cols])
+        return scaler, self.df
+
+
+class Sampling():
+    def __init__(self, x:pd.DataFrame,t:pd.Series):
+        self.x=x
+        self.t=t
 
     def under_sampling(self, factor=50):
-        counter = Counter(self.y)
-        minority_factor = counter[1]
-        rus = RandomUnderSampler(sampling_strategy={0:factor*minority_factor})
-        ux, uy= rus.fit_resample(self.x, self.y)
-        return ux, uy
-    
+        counter = Counter(self.t)
+        minority_count = counter[1]
+        majority_count = counter[0]
+
+        requested_majority = int(factor * minority_count)
+        requested_majority = min(requested_majority, majority_count)
+
+        rus = RandomUnderSampler(sampling_strategy={0: requested_majority,1: minority_count})
+        ux, ut = rus.fit_resample(self.x, self.t)
+        return ux, ut
+
 
     def over_sample(self, factor=50, strategy='smote', k_neig=3):
-        counter = Counter(self.y)
+        counter = Counter(self.t)
         majority_factor = counter[0]
-        if strategy=='random':
-            os = RandomOverSampler(random_state=RANDOM_STATE,sampling_strategy={1:majority_factor/factor})
+        if strategy == 'random':
+            os = RandomOverSampler(random_state=RANDOM_STATE, sampling_strategy={1: int(majority_factor / factor)})
         else:
-            os = SMOTE(random_state=RANDOM_STATE, sampling_strategy={1:majority_factor/factor}, k_neighbors=k_neig)
-
-        ox,oy = os.fit_resample(self.x, self.y)
-        return ox, oy
-    
+            os = SMOTE(random_state=RANDOM_STATE,
+                        sampling_strategy={1: int(majority_factor / factor)},
+                        k_neighbors=k_neig)
+        ox, ot = os.fit_resample(self.x, self.t)
+        return ox, ot
 
     def under_over_sample(self, under_factor=2, over_factor=20, strategy='smote', k_neig=3):
-        counter = Counter(self.y)
+        counter = Counter(self.t)
         majority_factor = counter[0]
 
-        if strategy=='random':
-            os = RandomOverSampler(random_state=RANDOM_STATE,sampling_strategy={1:majority_factor/over_factor})
+        if strategy == 'random':
+            os = RandomOverSampler(random_state=RANDOM_STATE, sampling_strategy={1: majority_factor / over_factor})
         else:
-            os = SMOTE(random_state=RANDOM_STATE, sampling_strategy={1:majority_factor/over_factor}, k_neighbors=k_neig)
+            os = SMOTE(random_state=RANDOM_STATE,
+                        sampling_strategy={1: int(majority_factor / over_factor)},
+                        k_neighbors=k_neig)
 
-        rus = RandomUnderSampler(sampling_strategy={0:majority_factor/under_factor})
+        rus = RandomUnderSampler(sampling_strategy={0: int(majority_factor / under_factor)})
 
-        from imblearn.pipeline import Pipeline
-        pip = Pipeline(steps=[('over',os),('under', rus)])
-
-        uox, uoy = pip.fit_resample(self.x, self.y)
-        return uox, uoy
-
-
+        pip = Pipeline(steps=[('over', os), ('under', rus)])
+        uox, uot = pip.fit_resample(self.x, self.t)
+        return uox, uot
 
 
-if __name__=='__main__':
-    df,x,t = load_data('data/split/train.csv',True)
-    print(Counter(t))  #Counter({0: 170579, 1: 305})
-    print(x.shape, t.shape)
-    preprocessing = Preprocessing(x,t)
-    x = preprocessing.remove_dublicates(True)
-    print(x.shape, t.shape)
-    x = preprocessing.change_time_to_hours('Time')
-    print(x.shape, t.shape)
-    x = preprocessing.handle_outliers(False)
-    print(x.shape, t.shape)
+
+    
+
+class Processing_Pipeline():
+    def __init__(self):
+        pass
+
+    def apply_preprocessing(self,path, scaling_option=1, scaler=True, val=False, remove_dublicate=True, remove_outlier=True, change_time=True):
+        df = load_df(path)
+        preprocessing = Preprocessing(df)
+        if remove_dublicate:
+            df=preprocessing.remove_duplicates()
+        if remove_outlier:
+            df = preprocessing.handle_outliers(True)
+        if change_time:
+            df = preprocessing.change_time_to_hours('Time')
+
+        return df
+
+    def apply_sampling(self,sample_option=1, under_factor=20, over_factor=20, over_strategy='smote'):
+        df = self.apply_preprocessing()
+        x,t = load_x_t(df)
+        sampling = Sampling()
+        
+        if sample==1:
+            x,t = sampling.under_sampling(under_factor)
+        elif sample==2:
+            x,t = sampling.over_sample(over_factor)
+        else:
+            x,t = sampling.under_over_sample(under_factor, over_factor,over_strategy)
+
+
+
+
 
--- utils/helper_fun.py
+++ utils/helper_fun.py
@@ -4,21 +4,24 @@ import pandas as pd
 
 RANDOM_STATE=42
 
-def load_data(path, train_only=True, split_sz=0.2):
+def load_df(path):
     if path is None:
         raise ValueError('Path is don`t defined correctly')
     df = pd.read_csv(path)
-    x,t = df.iloc[:,:-1], df.iloc[:,-1]
     
-    if train_only:
-        return df, x,t
-
-    else:
-        x_train, x_val, t_train, t_val = train_test_split(x,t, test_size=split_sz, random_state=RANDOM_STATE)
-        return df, x_train, x_val, t_train, t_val
+    return df
     
+def load_x_t(df:pd.DataFrame):
+    x,y = x,t = df.iloc[:,:-1], df.iloc[:,-1]
+    return df, x,y
 
+def split_data(x,t, split_sz=0.2):
+    x_train, x_val, t_train, t_val = train_test_split(x,t, test_size=split_sz, random_state=RANDOM_STATE)
+    return x_train, x_val, t_train, t_val
 
 if __name__=='__main__':
-    df,x,t = load_data('data/split/train.csv',True)
+    df= load_df('data/split/train.csv')
+    df,x,t=load_x_t(df)
+    print(df.shape, x.shape, t.shape)
+    x_train, x_val, t_train, t_val = split_data(x,t)
     print(df.shape, x.shape, t.shape)
